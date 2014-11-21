c9ac1cf5c4aae93ac78f44d76d8f6896140efe3e
falken1998 <tlrjlttl@gmail.com>
2014-11-21 13:16:56 -0700

    Save and loading

--- Random_funcs.py
+++ Random_funcs.py
@@ -1,1 +1,14 @@
-__author__ = 'Tyler'
+import pickle
+
+def save_file(list, file_name):
+    pickle_file = open(file_name,'w')
+    pickle.dump(list,pickle_file)
+    pickle_file.close()
+    return
+
+
+def load_file(file_name):
+    pickle_file = open(file_name, "r")
+    list = pickle.load(pickle_file)
+    pickle_file.close()
+    return list
